0034def019551fa367bddaa9a17197bce8be2df3
Waleed Aslam <m.walidaslam@gmail.com>
2021-08-26 23:20:33 +0500

    Initial

---
+++ MyProjectTemplate/MyProjectWizard/UserInputForm.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyProjectWizard
+{
+    public partial class UserInputForm : Form
+    {
+        private static string customMessage;
+        private static string devOps;
+        private static string PortNumber;
+        //private TextBox textBox1;
+        private Label label1;
+        private TextBox txtName;
+        private TextBox txtPort;
+        private Label lblName;
+        private Label lblport;
+        private Button btnCreate;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox txtDevOps;
+        private Label label7;
+        private Label label6;
+        //private Button button1;
+
+        public UserInputForm()
+        {
+            InitializeComponent();
+            //this.Size = new System.Drawing.Size(155, 265);
+
+            //button1 = new Button();
+            //button1.Location = new System.Drawing.Point(90, 25);
+            //button1.Size = new System.Drawing.Size(50, 25);
+            //button1.Click += button1_Click;
+            //this.Controls.Add(button1);
+
+            //textBox1 = new TextBox();
+            //textBox1.Location = new System.Drawing.Point(10, 25);
+            //textBox1.Size = new System.Drawing.Size(70, 20);
+            //this.Controls.Add(textBox1);
+        }
+        public static string CustomMessage
+        {
+            get
+            {
+                return customMessage;
+            }
+            set
+            {
+                customMessage = value;
+            }
+        }
+        public static string portNumber
+        {
+            get
+            {
+                return PortNumber;
+            }
+            set
+            {
+                PortNumber = value;
+            }
+        }
+        public static string DevOps
+        {
+            get
+            {
+                return devOps;
+            }
+            set
+            {
+                devOps = value;
+            }
+        }
+
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblport = new System.Windows.Forms.Label();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtDevOps = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Calibri", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(581, 66);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Add Custom Configurations";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(33, 205);
+            this.txtName.Multiline = true;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(557, 41);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPort.Location = new System.Drawing.Point(33, 323);
+            this.txtPort.Multiline = true;
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(556, 38);
+            this.txtPort.TabIndex = 2;
+            this.txtPort.TextChanged += new System.EventHandler(this.txtPort_TextChanged);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(30, 166);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(51, 21);
+            this.lblName.TabIndex = 3;
+            this.lblName.Text = "Name";
+            this.lblName.Click += new System.EventHandler(this.txtName_Click);
+            // 
+            // lblport
+            // 
+            this.lblport.AutoSize = true;
+            this.lblport.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblport.Location = new System.Drawing.Point(31, 286);
+            this.lblport.Name = "lblport";
+            this.lblport.Size = new System.Drawing.Size(43, 23);
+            this.lblport.TabIndex = 4;
+            this.lblport.Text = "Port";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCreate.Location = new System.Drawing.Point(936, 623);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(96, 43);
+            this.btnCreate.TabIndex = 5;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(28, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(196, 52);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Custom Template";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.Menu;
+            this.label3.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(234, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 21);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "C#";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.Menu;
+            this.label4.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(283, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 21);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Windows";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.Menu;
+            this.label5.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(377, 122);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 21);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Extension";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.Menu;
+            this.label6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label6.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(474, 122);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 21);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Custom";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtDevOps
+            // 
+            this.txtDevOps.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDevOps.Location = new System.Drawing.Point(34, 434);
+            this.txtDevOps.Multiline = true;
+            this.txtDevOps.Name = "txtDevOps";
+            this.txtDevOps.Size = new System.Drawing.Size(556, 39);
+            this.txtDevOps.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(38, 393);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(105, 23);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "DevOps URL";
+            // 
+            // UserInputForm
+            // 
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1108, 731);
+            this.ControlBox = false;
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtDevOps);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.lblport);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MinimizeBox = false;
+            this.Name = "UserInputForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.TransparencyKey = System.Drawing.Color.WhiteSmoke;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                customMessage = txtName.Text;
+                portNumber = txtPort.Text;
+                devOps = txtDevOps.Text;
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+           
+        }
+        private void txtPort_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
